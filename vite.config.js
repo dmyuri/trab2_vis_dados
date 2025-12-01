b7cde074813a5cc84bd403503c89c1976fcccd88
Yuri Moura <160618082+dmyuri@users.noreply.github.com>
2025-11-30 22:56:47 -0300

    fix: corrigindo caminho da pasta do dataset

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
-  publicDir: path.resolve(__dirname, '../00 - data'), // Serve the parent folder as public assets
+  publicDir: path.resolve(__dirname, './00 - data'), // Serve the parent folder as public assets
 });
 
